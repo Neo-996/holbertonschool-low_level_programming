e1f36bd4e04f5c28108c050e83671e3305107ce4
root <abdulelahmalshehri@gmail.com>
2025-02-18 17:45:27 +0000

    hotfix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
