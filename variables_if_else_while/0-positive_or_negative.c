f2b86959387dd64cac00955d97e99322dd73fc9b
root <abdulelahmalshehri@gmail.com>
2025-02-19 10:05:58 +0000

    hotfix

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 	  printf("%d is negative\n", n);
 	  }
-	else if (n >0)
+	else if (n > 0)
 	{
 	  printf("%d is positive\n", n);
 	}
-	else {
-	
-	  printf("is zero\n");
+	else
+	  {
+	  printf("%d is zero\n");
 	}
 	
 	return (0);
