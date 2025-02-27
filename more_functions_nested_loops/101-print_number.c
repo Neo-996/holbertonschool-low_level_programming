7a49be700c35f5c00afbc215f9a91b65a0665b2e
root <abdulelahmalshehri@gmail.com>
2025-02-27 15:12:08 +0000

    hotfix

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,22 +7,23 @@
   *
   * Return: Nothing!
   */
-void print_number(int n)
-{
-	unsigned int k = n;
+void print_number(int n) {
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
 
-	if (n < 0)
-	{
-		n *= -1;
-		k = n;
-		_putchar('-');
-	}
+    if (n / 10 != 0) {
+        print_number(n / 10);
+    }
 
-	k /= 10;
+    _putchar(n % 10 + '0');
+}
 
-	if (k != 0)
-		print_number(k);
-
-	_putchar((unsigned int) n % 10 + '0');
-
+int main() {
+    print_number(123);
+    _putchar('\n');
+    print_number(-456);
+    _putchar('\n');
+    return 0;
 }
