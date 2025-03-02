89bbcc8d5737044c57c4cc4304cbe2906d97d8ff
root <abdulelahmalshehri@gmail.com>
2025-03-02 19:49:48 +0000

    hotfix

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  * @s: Pointer to the memory area to be filled.
  * @b: The byte to fill the memory with.
  * @n: The number of bytes to be filled.
- * 
+ *
  * Return: A pointer to the memory area `s`.
  */
 char *_memset(char *s, char b, unsigned int n)
