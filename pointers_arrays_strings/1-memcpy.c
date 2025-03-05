65b664bbd8b2e4d0c0ea3fe2b12b041a2ce59f5d
root <abdulelahmalshehri@gmail.com>
2025-03-05 18:27:30 +0000

    hotfix

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,14 +10,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
 
-    /* Copy n bytes from src to dest */
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
-
-    /* Return the destination pointer */
-    return dest;
+return (dest);
 }
