8a17f335ab61e3e03748f1b07fa7104fffdccd18
root <abdulelahmalshehri@gmail.com>
2025-03-19 17:24:40 +0000

    hotfix

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func - Selects the correct operation function based on the operator.
  * @s: The operator passed as a string.
@@ -7,24 +8,26 @@
  * Return: A pointer to the corresponding function for the operator,
  * or NULL if no valid operator is found.
  */
+
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int i = 0;
 
-while (ops[i].op != NULL)
-{
-if (*s == *(ops[i].op))
-return ops[i].f;
-i++;
-}
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 
-return NULL;
+		i++;
+	}
+
+	return (0);
 }
