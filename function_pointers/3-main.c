21a80a1110d0932c1c0ddd610f20c96c43c4cd29
root <abdulelahmalshehri@gmail.com>
2025-03-19 17:29:11 +0000

    hotfix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - Performs simple operations based on user input
@@ -9,31 +11,22 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2;
-int (*op_func)(int, int);
+	int (*oprt)(int, int);
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-op_func = get_op_func(argv[2]);
+	oprt = get_op_func(argv[2]);
 
-if (op_func == NULL)
-{
-printf("Error\n");
-exit(99);
-}
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-{
-printf("Error\n");
-exit(100);
-}
-printf("%d\n", op_func(num1, num2));
-
-return 0;
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
